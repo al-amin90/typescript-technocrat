bc52b300a6da9ab0b7594ea2767d25f9f67aab5d
al-amin90 <al-amin90>
2025-11-28 22:31:30 +0600

    money bug problem not fixed

--- codeWars/problem3.ts
+++ codeWars/problem3.ts
@@ -6,4 +6,31 @@ export function narcissistic(value: number): boolean {
   return value === isNarcissistic
 }
 
-console.log('data', narcissistic(16));
+
+// return masked string
+export function maskify(cc: string): string {
+const maskLength = Math.max(0, cc.length-4)
+
+    return '#'.repeat(maskLength) + cc.substring(maskLength)
+}
+
+function sumUpNumbers(arr: string[]) {
+ const usFormatte = arr?.map(n => {
+  if(n.includes(',') && n.lastIndexOf(',') > n.lastIndexOf('.')){
+     return Number(n.replace(',', '.').replace('.', ''))
+    }else{
+      return Number(n.replace(',', ''))
+    }
+  })
+  const total = usFormatte?.reduce((a,c) => a +c,0)
+   const parts = total.toFixed(2).split('.');
+   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+   return parts.join('.');
+}
+
+// function sumUpNumbers(arr: string[]) {
+//   // your code here
+
+// }
+
+console.log('data', sumUpNumbers(["1,234.34", "1.324,2", "14"]));
