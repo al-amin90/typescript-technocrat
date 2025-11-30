cec9c4e717ea352ee7044be6f48fc5458659ec7f
al-amin90 <ijesun30@gmail.com>
2025-11-30 22:39:00 +0600

    module five revision

--- mongodb/partices1.js
+++ mongodb/partices1.js
@@ -6,3 +6,31 @@ db.test.find(
     }, 
     {company: 1, gender: 1, age: 1, interests: 1}).
     sort({ age:1 })
+
+    db.test.updateOne(
+    {_id: ObjectId("6406ad63fc13ae5a40000065"),},
+    { $pullAll: {friends: ["Fahim Ahammed Firoz", "Tanmoy Parvez" ]}})
+
+    db.test.updateOne(
+    {_id: ObjectId("6406ad63fc13ae5a40000065"),},
+    {$set: {'address.city':  'peyera',
+        'address.street': 'elu'
+    }}
+    )
+
+    db.test.updateOne(
+    {_id: ObjectId("6406ad63fc13ae5a40000065"), 'skills.isLearning': false},
+    {$set: {
+        'skills.$.isLearning':  null,
+    }}
+    )
+
+    db.test.deleteOne(
+    {_id: ObjectId("6406ad63fc13ae5a40000065"), 'skills.isLearning': true}
+    )
+
+db.test.find(
+    {age: {$gt: 30}}, {name: 1, email: 1}
+)
+
+
