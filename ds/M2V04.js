454df0d44eb3252e3a7716d4552c83ae965385f4
al-amin90 <ijesun30@gmail.com>
2026-04-30 18:16:40 +0600

    revision: some|array.form|range

--- ds/M2V04.js
+++ ds/M2V04.js
@@ -9,3 +9,13 @@ const featureAccessRoles = ["admin", "manager"];
 const canAccess = currentUserRoles.some((v) => featureAccessRoles.includes(v));
 
 //TODO Array.from()
+const arr = Array.from({ length: 5 }, (_, i) => i + 1); //? .fill(2)
+const arr2 = Array.from([1, 3, 5, 7], (value, i) => value * value);
+
+//TODO make range(python) in js
+const range = (start, stop, step) =>
+  Array.from(
+    { length: Math.ceil(stop - start) / step },
+    (_, i) => start + i * step,
+  );
+console.log(range(4, 8, 2));
