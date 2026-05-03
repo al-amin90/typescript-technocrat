2087a03b0bdf108ff6cc78e2b95acb4ac7aa82e2
al-amin90 <ijesun30@gmail.com>
2026-05-03 21:58:34 +0600

    revision: generic

--- src/moduel2/src/2.3.ts
+++ src/moduel2/src/2.3.ts
@@ -1,46 +1,42 @@
 {
-    
-    
-    // const rollNumbers: number[] = [2,45,5,65,6 ,4,3,]
-    // const mentors: string[] = ['x','y', 'z']
-    // const boolArray: boolean[] = [true, false, true]
+  // const rollNumbers: number[] = [2,45,5,65,6 ,4,3,]
+  // const mentors: string[] = ['x','y', 'z']
+  // const boolArray: boolean[] = [true, false, true]
 
-    // Generic Type ---> reuseable type korte parci....... use kore angle braclet user korbo
+  // Generic Type ---> reuseable type korte parci....... use kore angle braclet user korbo
 
-    const rollNumbers: Array<number> = [2, 45, 5, 65, 6, 4, 3,]
-    const mentors: Array<string> = ['x', 'y', 'z']
-    const boolArray: Array<boolean> = [true, false, true];
+  const rollNumbers: Array<number> = [2, 45, 5, 65, 6, 4, 3];
+  const mentors: Array<string> = ["x", "y", "z"];
+  const boolArray: Array<boolean> = [true, false, true];
 
-    type GenericArray<T> = Array<T>
-    
-    
-    const rollNumbers2: GenericArray<number> = [2, 45, 5, 65, 6, 4, 3,]
-    const mentors2: GenericArray<string> = ['x', 'y', 'z']
-    const boolArray2: GenericArray<boolean> = [true, false, true];
+  type GenericArray<T> = Array<T>;
 
-    const user : GenericArray<{name: string; age: number}> = [
-        {
-            name: "x",
-            age: 23
-        },
-        {
-            name: "y",
-            age: 345
-        }
-    ]
+  const rollNumbers2: GenericArray<number> = [2, 45, 5, 65, 6, 4, 3];
+  const mentors2: GenericArray<string> = ["x", "y", "z"];
+  const boolArray2: GenericArray<boolean> = [true, false, true];
 
+  const user: GenericArray<{ name: string; age: number }> = [
+    {
+      name: "x",
+      age: 23,
+    },
+    {
+      name: "y",
+      age: 345,
+    },
+  ];
+  // shariar nirjon
 
-    const add = (a: number, b: number) => a + b;
-    add(23, 2)
+  const add = (a: number, b: number) => a + b;
+  add(23, 2);
 
+  // Generic Type use kore Tuple ------------> Generic ar maddo me resuseable type use kora jai
+  type GenericTuple<X, Y> = [X, Y];
+  interface User {
+    name: string;
+    age: number;
+  }
 
-    // Generic Type use kore Tuple ------------> Generic ar maddo me resuseable type use kora jai
-    type GenericTuple<X, Y> = [x, Y]
-    interface User {name:string, age: number}
-    
-    const manus: [string, string] = ["x", "y"]
-    const UserWithId: GenericTuple<number, User> = [1, {name: "lalu", age:54}]
-
-
-
+  const manus: [string, string] = ["x", "y"];
+  const UserWithId: GenericTuple<number, User> = [1, { name: "lalu", age: 54 }];
 }
--- src/moduel2/src/2.5.ts
+++ src/moduel2/src/2.5.ts
@@ -1,51 +1,58 @@
 {
-// function with Generic
+  // function with Generic
 
-const creteArray = (param :string) : string[] => {
-    return [param]
-}
-const creteArrayWithGeneric = <T>(param: T) : T[] => {
-    return [param]
-}
+  const creteArray = (param: string) => {
+    return [param];
+  };
+  const creteArrayWithGeneric = <T>(param: T) => {
+    return [param];
+  };
 
-type User = {name: string; age: number}
+  type User = { name: string; age: number };
 
-const res1 = creteArray("bangladesh")
-const res2 = creteArrayWithGeneric<boolean>(true);
-const res3 = creteArrayWithGeneric<User>({name: "al amin", age: 23})
-console.log(res3);
+  const res1 = creteArray("bangladesh");
+  const res2 = creteArrayWithGeneric(true);
+  const res3 = creteArrayWithGeneric<User>({ name: "al amin", age: 23 });
+  console.log(res3);
 
-// function with Generic with Tuple
-const creteArrayWithGenericTuple = <T, Q>(param1: T, param2: Q): [T, Q] => {
-    return [param1, param2]
-}
-const res22 = creteArrayWithGenericTuple<string, number>("al amin", 232);
-const res33 = creteArrayWithGenericTuple<boolean, {isMarrid: boolean}>(true, {isMarrid: true});
-console.log(res33);
+  // function with Generic with Tuple
+  const creteArrayWithGenericTuple = <T, Q>(param1: T, param2: Q): [T, Q] => {
+    return [param1, param2];
+  };
+  const res22 = creteArrayWithGenericTuple("al amin", 232);
+  const res33 = creteArrayWithGenericTuple(true, { isMarrid: true });
+  console.log(res33);
 
+  const addCourseToStudent = <T>(student: T) => {
+    const course = "Next Level Web Development";
 
-
-
-const addCourseToStudent = <T>(student: T)  => {
-    const course = 'Next Level Web Development'
-    
     return {
-        ...student,
-        course
-    }
-}
+      ...student,
+      course,
+    };
+  };
 
-interface student1 {name: string, email: string, devType: string} 
-interface student2 {name: string, email: string, type: string}
+  interface student1 {
+    name: string;
+    email: string;
+    devType: string;
+  }
+  interface student2 {
+    name: string;
+    email: string;
+    type: string;
+  }
 
-const courseStudent = addCourseToStudent<student1>({
-    name: "x", email: "x@gmail.com", devType: "BLWD"
-})
-console.log(addCourseToStudent<student2>({
-    name: "x", email: "x@gmail.com", type: "Admin"
-}));
-
-
-
-
+  const courseStudent = addCourseToStudent({
+    name: "x",
+    email: "x@gmail.com",
+    devType: "BLWD",
+  });
+  console.log(
+    addCourseToStudent<student2>({
+      name: "x",
+      email: "x@gmail.com",
+      type: "Admin",
+    }),
+  );
 }
