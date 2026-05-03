c4c2b1fdbf7c8570752181f448c4239e1c58e306
al-amin90 <ijesun30@gmail.com>
2026-05-03 22:37:39 +0600

    revision: constraint with generic

--- src/moduel2/src/2.7.ts
+++ src/moduel2/src/2.7.ts
@@ -1,41 +1,39 @@
 {
-    // generic constraint with keyof operator
-    type Vehicle = {
-        bike: string;
-        car: string;
-        ship: string;
-    }
+  // generic constraint with keyof operator
+  type Vehicle = {
+    bike: string;
+    car: string;
+    ship: string;
+  };
 
-    type Owner = "bike" | "car" | "ship";
-    type Owner2 = keyof Vehicle;
-    const person1: Owner2 = "car"
+  type Owner = "bike" | "car" | "ship";
+  type Owner2 = keyof Vehicle;
+  const person1: Owner2 = "car";
 
-    // const getPropertyValue = <X, Y extends keyof X>(obj:X , key: Y) => {
-    //     return obj[key]
-    // }
+  // const getPropertyValue = <X, Y extends keyof X>(obj:X , key: Y) => {
+  //     return obj[key]
+  // }
 
-    const user = {
-        name: "al-amin",
-        age: 36,
-        address: "ctg"
-    }
+  const user = {
+    name: "al-amin",
+    age: 36,
+    address: "ctg",
+  };
 
-    const getPropertyValue = <X, Y extends keyof X >(obj: X, key: Y) => {
-        return obj[key]
-    }
-    
-    
-    const car = {
-        model: "Toyota",
-        year: 2303
-    }
-    const result1 = getPropertyValue(car, "year")
-    console.log(result1);
+  const getPropertyValue = <X>(obj: X, key: keyof X) => {
+    return obj[key];
+  };
 
-
-
-
-
-
-
+  const car = {
+    model: "Toyota",
+    year: 2303,
+  };
+  const student = {
+    id: 23,
+    name: "x",
+    email: "x@gmail.com",
+    devType: "BLWD",
+  };
+  const student1 = getPropertyValue(student, "devType");
+  const result1 = getPropertyValue(car, "year");
 }
