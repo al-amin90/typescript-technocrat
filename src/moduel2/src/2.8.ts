558e7e968f9a3eb2bafe44cdc1b4ed84c8751ef1
al-amin90 <ijesun30@gmail.com>
2026-05-04 12:08:48 +0600

    revision: as const

--- src/moduel2/src/2.8.ts
+++ src/moduel2/src/2.8.ts
@@ -1,53 +1,48 @@
 {
-    //Promise
-    type ToDo = { userId: number, id: number, title: string, completed: string }
+  //Promise
+  type ToDo = { userId: number; id: number; title: string; completed: string };
 
-    const getToDo = async() : Promise<ToDo> => {
-        const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-        const data = await response.json();
+  const getToDo = async (): Promise<ToDo> => {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1",
+    );
+    const data = await response.json();
 
-        return data
-    }
-    const data  =   getToDo()
-    console.log(data);
+    return data;
+  };
+  const data = getToDo();
+  console.log(data);
 
-    // Custom  Promise
-    type Something = {something: string}
-    // const createPromise = () : Promise<Something> => {
-    //     return new Promise<Something>((resolve, reject) => {
-    //         const data: Something = {something: "something"}
-    //         if(data){
-    //             resolve(data)
-    //         }else{
-    //             reject('failed to load data')
-    //         }
-    //     })
-    // }
+  // Custom  Promise
+  type Something = { something: string };
+  // const createPromise = () : Promise<Something> => {
+  //     return new Promise<Something>((resolve, reject) => {
+  //         const data: Something = {something: "something"}
+  //         if(data){
+  //             resolve(data)
+  //         }else{
+  //             reject('failed to load data')
+  //         }
+  //     })
+  // }
 
-    const createPromise = () : Promise<Something> => {
-        return new  Promise<Something> ((resolve, reject) => {
-            const data : Something  = {something: "something"}
+  const createPromise = (): Promise<Something> => {
+    return new Promise<Something>((resolve, reject) => {
+      const data: Something = { something: "something" };
 
-            if(data){
-                resolve(data)
-            }else{
-                reject("failed to load data")
-            }
-        })
-    }
+      if (data) {
+        resolve(data);
+      } else {
+        reject("failed to load data");
+      }
+    });
+  };
 
-    //calling create promise function
-    const showData = async () :  Promise<Something> => {
-        const data : Something = await createPromise();
-        // console.log(data, "D");
-        return data
-    }
-    showData()
-
-
-
-
-
-
-
+  //calling create promise function
+  const showData = async (): Promise<Something> => {
+    const data: Something = await createPromise();
+    // console.log(data, "D");
+    return data;
+  };
+  showData();
 }
--- src/moduel2/src/2.9.ts
+++ src/moduel2/src/2.9.ts
@@ -1,20 +1,35 @@
 {
-// conditional type
-type a1 = boolean;
-type b1 = undefined;
+  // as Constraint
 
-type x = a1 extends number ? true : false // conditional type
-type y = b1 extends boolean ? boolean : a1 extends number ? number : any
+  const UserRoles = {
+    Admin: "ADMIN",
+    User: "USER",
+    Editor: "EDITOR",
+  } as const;
 
-type Sheikh = {
+  const canEdit = (role: (typeof UserRoles)[keyof typeof UserRoles]) => {
+    // role: "ADMIN" | "USER" | "EDITOR"
+    if (role === UserRoles.Admin || role === UserRoles.Editor) {
+      return true;
+    } else return false;
+  };
+
+  const isEditPermission = canEdit(UserRoles.Admin);
+
+  // conditional type
+  type a1 = boolean;
+  type b1 = undefined;
+
+  type x = a1 extends number ? true : false; // conditional type
+  type y = b1 extends boolean ? boolean : a1 extends number ? number : any;
+
+  type Sheikh = {
     car: string;
     bike: string;
     ship: string;
-    plane: string
-}
+    plane: string;
+  };
 
-type checkVehicle<T> = T extends keyof Sheikh ? true : false
-type HasCar = checkVehicle<"plane"> 
-
-
+  type checkVehicle<T> = T extends keyof Sheikh ? true : false;
+  type HasCar = checkVehicle<"plane">;
 }
