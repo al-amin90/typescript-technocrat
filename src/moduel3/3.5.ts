448d67f191dc9597faa7318be4c65a6a21727fec
al-amin90 <ijesun30@gmail.com>
2026-05-05 15:22:43 +0600

    revision: Access Modifire

--- src/moduel3/3.5.ts
+++ src/moduel3/3.5.ts
@@ -1,60 +1,61 @@
-{
-    // access modifiers
-    class BankAccount{
-        public readonly id: number;
-        public name: string;
-        protected balance: number;
+// access modifiers
+class BankAccount {
+  public readonly userId: number;
+  public userName: string;
+  private userBalance: number;
 
-        constructor(id: number, name: string, balance: number){
-            this.id = id;
-            this.name = name;
-            this.balance = balance;
-        }
+  constructor(userId: number, userName: string, userBalance: number) {
+    this.userId = userId;
+    this.userName = userName;
+    this.userBalance = userBalance;
+  }
 
-        public addDiposit(money: number){
-            return this.balance = this.balance + money
-        }
-        public getDiposit(){
-            return this.balance
-        }
-    }
-
-    const goribManusherAccount = new BankAccount(111, "Mr. gorib", 20)
-    // console.log(goribManusherAccount.addDiposit(400));
-
-    class StudentAccount extends BankAccount{
-        constructor(id: number, name: string, balance: number){
-            super(id, name, balance)
-        }
-        test(){
-            // console.log(this.balance);
-        }
-    }
-    const student = new StudentAccount(333, "al amin", 300)
-    // student.
+  public addBalance(money: number) {
+    return (this.userBalance = this.userBalance + money);
+  }
+  public getBalance() {
+    return this.userBalance;
+  }
 }
 
-{ //----------------------------------------------------------------
-    class BankAccount {
+const mezbaVaiAccount = new BankAccount(111, "Mr. gorib", 20);
 
-        constructor ( readonly id: number, public name: string, protected balance: number){}
+class StudentAccount extends BankAccount {
+  constructor(id: number, name: string, balance: number) {
+    super(id, name, balance);
+  }
+  test() {
+    // console.log(this.balance);
+  }
+}
+const student = new StudentAccount(333, "al amin", 300);
+// student.
 
-        addDeposit(amount: number){
-            this.balance = this.balance + amount
-        }
-        getBalance(){
-            return this.balance
-        }
+{
+  //----------------------------------------------------------------
+  class BankAccount {
+    constructor(
+      readonly id: number,
+      public name: string,
+      protected balance: number,
+    ) {}
+
+    addDeposit(amount: number) {
+      this.balance = this.balance + amount;
     }
-
-    class StudentAccount extends BankAccount {
-        test() {
-            this.balance
-        }
+    getBalance() {
+      return this.balance;
     }
+  }
 
-    const goribManusherAccount = new BankAccount(111, "Mr. gorib", 20)
+  class StudentAccount extends BankAccount {
+    test() {
+      this.balance;
+    }
+  }
 
-    goribManusherAccount.addDeposit(10)
-    console.log(goribManusherAccount.getBalance());
+  const goribManusherAccount = new BankAccount(111, "Mr. gorib", 20);
+
+  goribManusherAccount.addDeposit(10);
+  console.log(goribManusherAccount.getBalance());
 }
