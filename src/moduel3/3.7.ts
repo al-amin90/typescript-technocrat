91d6f9f58b92e93edd1c8183a07ec5673183fa8d
al-amin90 <ijesun30@gmail.com>
2026-05-05 19:29:13 +0600

    revision: setter getter  &  Static

--- src/moduel3/3.7.ts
+++ src/moduel3/3.7.ts
@@ -1,51 +1,47 @@
 {
-    // static in OOP
-    // ar ak holo single memory source theke control kora, alada instace hoileo single source theke kaj kore.
-    class Counter {
-        static count: number = 0;
-        static increment() {
-            return Counter.count = Counter.count + 1
-        }
-        decrement() {
-            return Counter.count = Counter.count - 1
-        }
+  // static in OOP
+  // ar ak holo single memory source theke control kora, alada instace hoileo single source theke kaj kore.
+  class Counter {
+    static count: number = 0;
+    increment() {
+      return (Counter.count = Counter.count + 1);
     }
+    decrement() {
+      return (Counter.count = Counter.count - 1);
+    }
+  }
 
-    // console.log(Counter.increment()); // 1 -> different memory
+  // console.log(Counter.increment()); // 1 -> same memory
 
-    const instance2 = new Counter()
-    // console.log(instance2.decrement()); // -1 -> different memory
-
+  const instance2 = new Counter();
+  // console.log(instance2.decrement()); // -1 -> same memory
 }
 
+{
+  //----------------------------------------------------------------------
+  class Counter {
+    static count: number = 0;
+    static increment() {
+      return (Counter.count = Counter.count + 1);
+    }
+    static decrement() {
+      return (Counter.count = Counter.count - 1);
+    }
+  }
 
-{ //----------------------------------------------------------------------
-    class Counter {
-        static count: number = 0
-        static increment() {
-            return Counter.count = Counter.count + 1
-        }
-        static decrement() {
-            return Counter.count = Counter.count - 1
-        }
-}
+  // const instance1 = new Counter();
+  Counter.increment();
+  Counter.increment();
+  Counter.increment();
+  Counter.increment();
+  Counter.increment();
+  Counter.increment();
+  Counter.increment();
+  console.log(Counter.increment()); // 1 -> same memory
 
-// const instance1 = new Counter();
-Counter.increment()
-Counter.increment()
-Counter.increment()
-Counter.increment()
-Counter.increment()
-Counter.increment()
-Counter.increment()
-console.log(Counter.increment()); // 1 -> different memory
-
-// const instance2 = new Counter()
-Counter.decrement()
-Counter.decrement()
-Counter.decrement()
-console.log(Counter.decrement()); // -1 -> different memory
-
-
-
+  // const instance2 = new Counter()
+  Counter.decrement();
+  Counter.decrement();
+  Counter.decrement();
+  console.log(Counter.decrement()); // -1 -> same memory
 }
