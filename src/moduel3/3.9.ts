fccef0dab59973f475626deae4eba4a2bd16994e
al-amin90 <ijesun30@gmail.com>
2026-05-05 19:48:26 +0600

    revision: polymorfijom|Encapsulation|Abstraction

--- src/moduel3/3.9.ts
+++ src/moduel3/3.9.ts
@@ -1,107 +1,108 @@
 {
-    //abstraction 1. interface    2.abstract
-    //idea
-    interface Vehicle {
-        startEngine(): void;
-        stopEngine(): void;
-        move(): void;
+  //abstraction 1. interface    2.abstract
+  //idea
+  interface Vehicle {
+    startEngine(): void;
+    stopEngine(): void;
+    move(): void;
+  }
+
+  // akane interface ar oi structure ta car class follow korbe , atai Abstraction ja ki hote pare tar akti avas dei
+
+  class Car implements Vehicle {
+    startEngine(): void {
+      console.log("the car is starting the car engine");
     }
-
-    // akane interface ar oi structure ta car class follow korbe , atai Abstraction ja ki hote pare tar akti avas dei 
-
-    class Car implements Vehicle{
-        startEngine(): void{
-            console.log("the car is starting the car engine");
-        }
-        stopEngine(): void {
-            console.log("the car is stop the engine");
-        }
-        move(): void {
-            console.log("the car is moving");
-        }
-        test(): void {
-            console.log("i am testing");
-        }
+    stopEngine(): void {
+      console.log("the car is stop the engine");
     }
-
-    const toyotaCar = new Car()
-    toyotaCar.stopEngine()
-
-    // Abstract class
-    // abstract class theke tumi follow korte parbe, kintu create korte parbe na atai instance make hoi na;
-        //idea
-    abstract class Car2 {
-        // ki hote pare idea dite parbe, kisu impletation korte parbe na
-        abstract startEngine(): void;  
-        abstract stopEngine(): void;
-        abstract move(): void;
-        abstract test(): void;
+    move(): void {
+      console.log("the car is moving");
     }
-
-    //real Implementation
-    class ToyotaCar extends Car2{
-        startEngine(): void{
-            console.log("the car is starting the car engine");
-        }
-        stopEngine(): void {
-            console.log("the car is stop the engine");
-        }
-        move(): void {
-            console.log("the car is moving");
-        }
-        test(): void {
-            console.log("i am testing");
-        }
+    test(): void {
+      console.log("i am testing");
     }
+  }
+
+  const toyotaCar = new Car();
+  toyotaCar.stopEngine();
+
+  // Abstract class
+  // abstract class theke tumi follow korte parbe, kintu create korte parbe na atai instance make hoi na;
+  //idea
+  abstract class Car2 {
+    // ki hote pare idea dite parbe, kisu impletation korte parbe na
+    abstract startEngine(): void;
+    abstract stopEngine(): void;
+    abstract move(): void;
+    abstract test(): void;
+  }
+
+  //real Implementation
+  class ToyotaCar extends Car2 {
+    startEngine(): void {
+      console.log("the car is starting the car engine");
+    }
+    stopEngine(): void {
+      console.log("the car is stop the engine");
+    }
+    move(): void {
+      console.log("the car is moving");
+    }
+    test(): void {
+      console.log("i am testing");
+    }
+  }
 }
 
-{// --------------------------------------------------------------------
+{
+  // --------------------------------------------------------------------
 
-    interface Vehicle1 {
-        startEngine(): void;
-        stopEngine(): void;
-        move(): void;
+  interface Vehicle1 {
+    startEngine(): void;
+    stopEngine(): void;
+    move(): void;
+  }
+
+  class Car implements Vehicle1 {
+    startEngine(): void {
+      console.log("the car is starting the car engine");
     }
-
-    class Car implements Vehicle1 {
-        startEngine(): void{
-            console.log("the car is starting the car engine");
-        }
-        stopEngine(): void {
-            console.log("the car is stop the engine");
-        }
-        move(): void {
-            console.log("the car is moving");
-        }
-        test(): void {
-            console.log("i am testing");
-        }
+    stopEngine(): void {
+      console.log("the car is stop the engine");
     }
-
-    const toyotaCar = new Car()
-    toyotaCar.stopEngine()
-
-    abstract class Vehicle2 {
-        abstract startEngine(): void;
-        abstract stopEngine(): void;
-        abstract move(): void;
-        abstract test(): void;
+    move(): void {
+      console.log("the car is moving");
     }
-
-    class Car2 extends Vehicle2 {
-        startEngine(): void{
-            console.log("the car is starting the car engine");
-        }
-        stopEngine(): void {
-            console.log("the car is stop the engine");
-        }
-        move(): void {
-            console.log("the car is moving");
-        }
-        test(): void {
-            console.log("i am testing");
-        }
+    test(): void {
+      console.log("i am testing");
     }
-    const hondaCar = new Car2()
-    hondaCar.move()
+  }
+
+  const toyotaCar = new Car();
+  toyotaCar.stopEngine();
+
+  abstract class Vehicle2 {
+    abstract startEngine(): void;
+    abstract stopEngine(): void;
+    abstract move(): void;
+    abstract test(): void;
+  }
+
+  class Car2 extends Vehicle2 {
+    startEngine(): void {
+      console.log("the car is starting the car engine");
+    }
+    stopEngine(): void {
+      console.log("the car is stop the engine");
+    }
+    move(): void {
+      console.log("the car is moving");
+    }
+    test(): void {
+      console.log("i am testing");
+    }
+  }
+  const hondaCar = new Car2();
+  hondaCar.move();
 }
